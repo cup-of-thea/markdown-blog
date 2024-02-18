17ba1309c1f018367fa18d6a90dafb1f137488fc
Thea <thea.joy@outlook.com>
2024-02-18 17:10:24 +0100

    feat: add post tags

--- src/Domain/UseCases/Commands/LinkTaxonomiesCommand.php
+++ src/Domain/UseCases/Commands/LinkTaxonomiesCommand.php
@@ -10,13 +10,14 @@ class LinkTaxonomiesCommand
     public function link(MarkdownPost $post): void
     {
         $this->linkCategory($post);
+        $this->linkTags($post);
     }
 
     /**
      * @param MarkdownPost $post
      * @return void
      */
-    public function linkCategory(MarkdownPost $post): void
+    private function linkCategory(MarkdownPost $post): void
     {
         if (!empty($post->meta->category)) {
             $categorySlug = str($post->meta->category)->slug();
@@ -34,5 +35,37 @@ class LinkTaxonomiesCommand
                 ->where('slug', $post->meta->slug)
                 ->update(['category_id' => $categoryId]);
         }
+    }
+
+    private function linkTags(MarkdownPost $post): void
+    {
+        if (!empty($post->meta->tags)) {
+            $tagIds = collect($post->meta->tags)
+                ->map(fn($tag) => str($tag)->slug())
+                ->map(fn($tag) => $this->findOrCreateTag($tag))
+                ->toArray();
+
+            $postId = DB::table('posts')->where('slug', $post->meta->slug)->first()->id;
+
+            DB::table('post_tag')->where('post_id', $postId)->delete();
+
+            DB::table('post_tag')->insert(
+                collect($tagIds)
+                    ->map(fn($tagId) => ['post_id' => $postId, 'tag_id' => $tagId])
+                    ->toArray()
+            );
+        }
+    }
+
+    private function findOrCreateTag($tag)
+    {
+        $query = DB::table('tags')->where('slug', $tag);
+
+        $query->count() ?: $query->insert([
+            'title' => $tag,
+            'slug' => $tag,
+        ]);
+
+        return $query->first()->id;
     }
 }
